9b3a0a18d2034131ead72da9ff00d7952778f7ae
Chenlei Fu <chenlei.fu@outlook.com>
2021-08-07 18:46:37 -0500

    first commit

---
+++ BackEnd/stem-education-backend/src/main/java/com/example/stemeducationbackend/StemEducationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.stemeducationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StemEducationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StemEducationBackendApplication.class, args);
+	}
+
+}
---
+++ BackEnd/stem-education-backend/src/test/java/com/example/stemeducationbackend/StemEducationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stemeducationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StemEducationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
