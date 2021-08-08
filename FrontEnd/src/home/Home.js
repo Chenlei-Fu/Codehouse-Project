e48bb1c0157498019417c50c643d3dc89a7bbf43
Chenlei Fu <chenlei.fu@outlook.com>
2021-08-08 12:51:30 -0500

    finish login and signup page

---
+++ FrontEnd/src/home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+class Home extends Component {
+    
+    render() {
+        return (
+            <div className="home-container">
+                <div className="container">
+                    <h1 className="home-title">Home Page</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
